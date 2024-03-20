2ff72f6af9414f846ce535e80014332be3b5bd3a
grachev21 <grachev613@gmail.com>
2024-03-20 07:18:19 +0400

    status_06

--- guitar_school/templatetags/TagConfig.py
+++ guitar_school/templatetags/TagConfig.py
@@ -4,15 +4,19 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('guitar_school/tag_templates/line.html')
-def line(**kwargs):
+def line():
     pass
 
 
 @register.inclusion_tag('guitar_school/tag_templates/logo.html')
-def logo(**kwargs):
+def logo():
     pass
 
 
 @register.inclusion_tag('guitar_school/tag_templates/icons.html')
-def icons(**kwargs):
-    pass
+def icons(color):
+    data = {
+        "color": color
+    }
+
+    return data
